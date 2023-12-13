50e48ef2b83c41429d108bcb6ce1d75d13e5b678
VolodymyrBab <108554912+VolodymyrBab@users.noreply.github.com>
2023-12-13 16:07:25 +0000

    Deploying to gh-pages from @ VolodymyrBab/goit-react-hw-05-movies@58dd5c83d7c7b8dc5edce3e0fbfea97257d902a6 🚀

---
+++ static/js/256.bafa91eb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(791),c=n(689),u=n(184);function o(){var e=(0,c.s0)();return(0,r.useEffect)((function(){setTimeout((function(){e("/",{replace:!0})}),3e3)}),[e]),(0,u.jsxs)("div",{children:[(0,u.jsx)("h1",{children:"OOOps! Sorry, we couldn't find that page :("}),(0,u.jsx)("h2",{children:"We will move you to the main page"})]})}}}]);
+//# sourceMappingURL=256.bafa91eb.chunk.js.map
